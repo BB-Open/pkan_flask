d72364104db24f0835bb9a7d9e2da31c85942d27
sandra <sandra.rum@inqbus.de>
2019-04-15 17:39:30 +0200

    Initial Commit with first running example

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pkan_flask',
+    version='',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['pkan',
+                        'pkan.flask',
+                        ],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='sandra',
+    author_email='',
+    description='',
+    install_requires = [
+        'flask',
+        'flask-socketio',
+        'eventlet',
+        # 'psycopg2',
+        # 'sqlalchemy',
+        'gunicorn',
+        'simplejson',
+        # 'xarray',
+        # 'ipaddress',
+        'colorlog',
+    ],
+)
