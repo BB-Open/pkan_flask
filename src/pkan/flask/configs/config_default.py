d07fd137df18986fa6b7b1aa5fe467965aa477d9
sandra <sandra.rum@inqbus.de>
2019-12-05 11:28:35 +0100

    Added Labelfields

--- src/pkan/flask/configs/config_default.py
+++ src/pkan/flask/configs/config_default.py
@@ -49,11 +49,14 @@ DESCRIPTION_PREFIXES = [
 
 LABEL_FIELDS = [
     'dct:title',
-
+    'skos:prefLabel',
+    'rdfs:label',
 ]
 
 LABEL_PREFIXES = [
     'dct: <http://purl.org/dc/terms/>',
+    'skos: <http://www.w3.org/2004/02/skos/core#>',
+    'rdfs: <http://www.w3.org/2000/01/rdf-schema#>'
 ]
 
 TRANSLATION_SPARQL_QUERY_RAW = """
