409594023e088e58f7fe8679616253167e0ddb23
sandra <sandra.rum@inqbus.de>
2020-01-23 11:29:37 +0100

    Added query for datasets and catalogs. Missing: How to deal with Sparql.

--- src/pkan/flask/sparql_db.py
+++ src/pkan/flask/sparql_db.py
@@ -1,13 +1,10 @@
 """
 DB Manager for Sparql Queries
 """
-from pkan.flask.configs.config_default import (
-    BATCH_SIZE, PLONE_DCAT_NAMESPACE,
-    PLONE_SKOS_CONCEPT_NAMESPACE,
-    PLONE_ALL_OBJECTS_NAMESPACE, TITLE_PREFIXES, TITLE_FIELDS,
-    DESCRIPTION_PREFIXES, DESCRIPTION_FIELDS,
-    TITLE_QUERY_LANG, LABEL_PREFIXES, LABEL_FIELDS, TITLE_QUERY,
-    BLAZEGRAPH_BASE, )
+try:
+    import pkan.flask.configs.config as cfg
+except ImportError:
+    import pkan.flask.configs.config_default as cfg
 from pkan.flask.log import LOGGER
 
 from pkan.blazegraph.api import Tripelstore, SPARQL
@@ -22,7 +19,7 @@ class DBManager():
         """
         Init
         """
-        self.tripel_store = Tripelstore(BLAZEGRAPH_BASE)
+        self.tripel_store = Tripelstore(cfg.BLAZEGRAPH_BASE)
         self.tripel_store.generate_namespace_uri('govdata')
 
     def get_category_vocab(self):
@@ -44,7 +41,7 @@ class DBManager():
             }
         """
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_SKOS_CONCEPT_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_SKOS_CONCEPT_NAMESPACE)
         res = sparql.query(sparql_query)
 
         data = []
@@ -61,16 +58,6 @@ class DBManager():
 
         return data
 
-    def category_id_to_sparql(self, _params):
-        """
-        Build up the sparql filter to add to sparql query
-        :param title:
-        :return:
-        """
-        # todo
-
-        return ''
-
     def get_file_format_vocab(self):
         """
         Provide a vocab including the categories
@@ -86,7 +73,7 @@ class DBManager():
             }
         """
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_DCAT_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_DCAT_NAMESPACE)
         res = sparql.query(sparql_query)
 
         data = []
@@ -101,15 +88,6 @@ class DBManager():
             })
 
         return data
-
-    def file_format_id_to_sparql(self, _params):
-        """
-        Build up the sparql filter to add to sparql query
-        :param title:
-        :return:
-        """
-        # todo
-        return ''
 
     def get_publisher_vocab(self):
         """
@@ -126,7 +104,7 @@ class DBManager():
             }
                 """
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_DCAT_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_DCAT_NAMESPACE)
         res = sparql.query(sparql_query)
 
         data = []
@@ -141,15 +119,6 @@ class DBManager():
             })
 
         return data
-
-    def publisher_id_to_sparql(self, _params):
-        """
-        Build up the sparql filter to add to sparql query
-        :param title:
-        :return:
-        """
-        # todo
-        return ''
 
     def get_license_vocab(self):
         """
@@ -166,7 +135,7 @@ class DBManager():
             }
         """
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_DCAT_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_DCAT_NAMESPACE)
         res = sparql.query(sparql_query)
 
         data = []
@@ -182,33 +151,6 @@ class DBManager():
 
         return data
 
-    def license_id_to_sparql(self, _params):
-        """
-        Build up the sparql filter to add to sparql query
-        :param title:
-        :return:
-        """
-        # todo
-        return ''
-
-    def keywords_to_sparql(self, _params):
-        """
-        Build up the sparql filter to add to sparql query
-        :param title:
-        :return:
-        """
-        # todo
-        return ''
-
-    def last_change_date_to_sparql(self, _params):
-        """
-        Build up the sparql filter to add to sparql query
-        :param _params:
-        :return:
-        """
-        # todo
-        return ''
-
     def get_sorting_options(self):
         """
         Get a Vocab with the Sorting Options like Newest
@@ -217,7 +159,7 @@ class DBManager():
         # todo: which options are suported?
         return [
             {'text': 'Relevanz',
-             'id': 'relevance',
+             'id': 'score_desc',
              'icon_class': None},
             {'text': 'Datum aufsteigend',
              'id': 'date_asc',
@@ -226,21 +168,227 @@ class DBManager():
              'id': 'date_desc',
              'icon_class': None},
             {'text': 'Alphabetisch aufsteigend',
-             'id': 'letter_asc',
+             'id': 'title_asc',
              'icon_class': None},
             {'text': 'Alphabetisch absteigend',
-             'id': 'letter_desc',
+             'id': 'title_desc',
              'icon_class': None},
         ]
 
-    def sorting_option_to_sparql(self, _params):
+    def sorting_option_to_sparql(self, params):
         """
         Build up the sparql filter to add to sparql query
         :param title:
         :return:
         """
-        # todo
-        return ''
+        if params['order_by']:
+            field, order = params['order_by'].split('_')
+        else:
+            field, order = 'score', 'desc'
+        order_by = """ORDER BY {order}(?{field})desc(?default_score)""".format(field=field, order=order)
+
+        return order_by
+
+    def get_namespaces(self):
+        namespaces = """
+prefix dcat: <http://www.w3.org/ns/dcat#>
+prefix dct: <http://purl.org/dc/terms/>
+prefix foaf: <http://xmlns.com/foaf/0.1/>
+prefix bds: <http://www.bigdata.com/rdf/search#>"""
+        return namespaces
+
+    def get_values(self, params):
+        #  'order_by': 'date_asc', 'sparql': '', 'batch_start': 0, 'batch_end': 1, 'last_change': ['2020-01-02T13:35:00.000Z', '2020-01-04T13:35:00.000Z']}
+        query = ''
+
+        neg_pos_fields = ['file_format', 'category', 'publisher', 'license']
+
+        for field in neg_pos_fields:
+            if params[field]:
+                if params[field]['value_pos']:
+                    values = '<' + '> <'.join(params[field]['value_pos']) + '>'
+                    query += '\nVALUES ?' + field + '_pos {' + values + '} .'
+                if params[field]['value_neg']:
+                    values = '<' + '> <'.join(params[field]['value_neg']) + '>'
+                    query += '\nVALUES ?' + field + '_neg {' + values + '} .'
+
+        return query
+
+    def get_search_results_dataset(self, params, values):
+        query = ''
+        # params example
+
+        # SELECT
+        select = """\nSELECT DISTINCT ?id ?date ?title ?default_score"""
+        query += select
+
+        # WHERE
+        where = """
+                WHERE {"""
+        query += where
+
+        # Values
+        # VALUES ?type { dcat:Dataset dcat:Catalog } .
+        query += values
+
+        # WHERE TYPE
+        where_base_fields = """
+            ?id a dcat:Dataset .
+            ?id dct:title ?title .
+            ?id dct:modified ?date ."""
+
+        query += where_base_fields
+
+        filters = """"""
+
+        if 'publisher' in values:
+            filters += """
+            ?catalog a dcat:Catalog .
+            ?catalog dcat:dataset ?id ."""
+            if 'publisher_pos' in values:
+                filters += """
+            ?catalog dct:publisher ?publisher_pos ."""
+            if 'publisher_neg' in values:
+                filters += """
+            FILTER(NOT EXISTS { ?catalog dct:publisher ?publisher_neg . })"""
+
+        if 'license' in values or 'file_format' in values:
+            filters += """
+            ?distribution a dcat:Distribution .
+            ?id dcat:distribution ?distribution ."""
+            if 'license_pos' in values:
+                filters += """
+            ?distribution dct:license ?license_pos ."""
+            if 'license_neg' in values:
+                filters += """
+            FILTER(NOT EXISTS { ?distribution dct:license ?license_neg . })"""
+            if 'file_format_pos' in values:
+                filters += """
+            ?distribution dct:format ?file_format_pos ."""
+            if 'file_format_neg' in values:
+                filters += """
+            FILTER(NOT EXISTS { ?distribution dct:license ?file_format_neg . })"""
+        
+        if 'category' in values:
+            if 'category_pos' in values:
+                filters += """
+                ?id dcat:theme ?category_pos ."""
+            if 'category_neg' in values:
+                filters += """
+                FILTER(NOT EXISTS { ?id dcat:theme ?category_neg . })"""
+
+        if params['last_change']:
+            start, end = params['last_change']
+            if start:
+                filters += '''
+            FILTER(?date >= "''' + start + '''"^^xsd:dateTime )'''
+            if end:
+                filters += '''
+            FILTER(?date >= "''' + end + '''"^^xsd:dateTime )'''
+
+        query += filters
+
+        # we set a store to order results which are placed in the same position
+        query += '''
+                    bind( 0.7 as ?default_score)'''
+
+        # todo: how to deal with sparql
+
+        # WHERE_END
+        where_end = """
+        }"""
+        query += where_end
+
+        return query
+    
+    def get_search_results_catalog(self, params, values):
+        query = ''
+        # params example
+
+        # SELECT
+        select = """\nSELECT DISTINCT ?id ?date ?title ?default_score"""
+        query += select
+
+        # WHERE
+        where = """
+                WHERE {"""
+        query += where
+
+        # Values
+        # VALUES ?type { dcat:Dataset dcat:Catalog } .
+        query += values
+
+        # WHERE TYPE
+        where_base_fields = """
+            ?id a dcat:Catalog .
+            ?id dct:title ?title .
+            ?id dct:modified ?date ."""
+
+        query += where_base_fields
+
+        filters = """"""
+
+        if 'publisher' in values:
+            if 'publisher_pos' in values:
+                filters += """
+            ?id dct:publisher ?publisher_pos ."""
+            if 'publisher_neg' in values:
+                filters += """
+            FILTER(NOT EXISTS { ?id dct:publisher ?publisher_neg . })"""
+
+        if 'license' in values or 'file_format' in values:
+            filters += """
+            ?dataset a dcat:Dataset .
+            ?id dcat:dataset ?dataset .
+            ?distribution a dcat:Distribution .
+            ?dataset dcat:distribution ?distribution ."""
+            if 'license_pos' in values:
+                filters += """
+            ?distribution dct:license ?license_pos ."""
+            if 'license_neg' in values:
+                filters += """
+            FILTER(NOT EXISTS { ?distribution dct:license ?license_neg . })"""
+            if 'file_format_pos' in values:
+                filters += """
+            ?distribution dct:format ?file_format_pos ."""
+            if 'file_format_neg' in values:
+                filters += """
+            FILTER(NOT EXISTS { ?distribution dct:license ?file_format_neg . })"""
+
+        if 'category' in values:
+            filters += """
+                ?dataset a dcat:Dataset .
+                ?id dcat:dataset ?dataset ."""
+            if 'category_pos' in values:
+                filters += """
+                ?dataset dcat:theme ?category_pos ."""
+            if 'category_neg' in values:
+                filters += """
+                FILTER(NOT EXISTS { ?dataset dcat:theme ?category_neg . })"""
+
+        if params['last_change']:
+            start, end = params['last_change']
+            if start:
+                filters += '''
+            FILTER(?date >= "''' + start + '''"^^xsd:dateTime )'''
+            if end:
+                filters += '''
+            FILTER(?date >= "''' + end + '''"^^xsd:dateTime )'''
+
+        query += filters
+
+        # we set a store to order results which are placed in the same position
+        query += '''
+            bind( 0.8 as ?default_score)'''
+
+        # todo: how to deal with sparql
+
+        # WHERE_END
+        where_end = """
+        }"""
+        query += where_end
+
+        return query
 
     def get_search_results(self, params):
         """
@@ -248,96 +396,79 @@ class DBManager():
         :param params:
         :return:
         """
-
+        values = self.get_values(params)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_ALL_OBJECTS_NAMESPACE)
         query = ''
 
         # Namespaces
-
-        namespaces = """
-prefix dcat: <http://www.w3.org/ns/dcat#>
-prefix dct: <http://purl.org/dc/terms/>"""
-        query += namespaces
+        query += self.get_namespaces()
 
         # SELECT
-        select = """
-SELECT ?dataset ?date"""
-        query += select
 
-        # WHERE
-        where = """
-WHERE {"""
-        query += where
+        query +="""
+SELECT DISTINCT ?id ?date ?title ?score ?default_score WHERE {
+{ """
+        # SUBQUERIES
 
-        # WHERE TYPE
-        where_type = """
-    ?dataset a dcat:Dataset ."""
+        query += self.get_search_results_dataset(params, values)
 
-        query += where_type
+        query += '} UNION {'
 
-        # FILTER for Category
-        where_category = """
-    ?dataset dcat:theme <{category}> ."""
+        # todo: replace by catalog if implemented
+        query += self.get_search_results_catalog(params, values)
 
-        if 'category' in params:
-            if len(params['category']['value_pos']) == 1 :
-                query += where_category.format(
-                        category = params['category']['value_pos'][0]
-                )
+        query += '}'
 
-        # WHERE Date
-        where_date = """
-    ?dataset dct:modified ?date . """
-        query += where_date
+        # KEYWORDS
 
-        # WHERE_END
-        where_end = """
-}"""
-        query += where_end
+        if params['textline_keywords']:
+            query += '''
+            ?o bds:search "''' + params['textline_keywords'] + '''" .
+            ?o bds:relevance ?score .
+            ?o bds:matchAllTerms "false" .
+          	?o bds:minRelevance "0.25" .
+            ?id ?p ?o .'''
 
-        # ORDER BY (date)
-        default_order_by = """
-ORDER BY (?date)"""
-        order_by = """
-ORDER BY {order}(?date)"""
+        query += '}'
 
-        if params['order_by']:
-            query += order_by
-        else :
-            query += default_order_by
+        query += self.sorting_option_to_sparql(params)
+
+        # we need this to know how many pages of results we have
+        all_res = sparql.query(query)
 
         # BATCHING
         limit = """
-LIMIT {limit}
-OFFSET {offset}
-"""
-        batch_start = params['batch_start'] * BATCH_SIZE
-        batch_end = params['batch_end'] * BATCH_SIZE
+                LIMIT {limit}
+                OFFSET {offset}
+                """
+        batch_start = params['batch_start'] * cfg.BATCH_SIZE
+        batch_end = params['batch_end'] * cfg.BATCH_SIZE
 
         query += limit.format(
-               limit = batch_end - batch_start + 1,
-               offset = batch_start
+            limit=batch_end - batch_start + 1,
+            offset=batch_start
         )
 
-        print(query)
-
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_ALL_OBJECTS_NAMESPACE)
-
         res = sparql.query(query)
+
+        LOGGER.info('Execute Sparql')
+        LOGGER.info(query)
 
         data = []
 
         for obj in res.bindings:
-            obj_uri = obj['dataset'].value
+            obj_uri = obj['id'].value
+            obj_title = obj['title'].value
             data.append({
                 'id': obj_uri,
-                'title': self.get_title(obj_uri),
+                'title': obj_title,
                 'description': self.get_description(obj_uri),
                 'type': self.get_type(obj_uri)
             })
 
         LOGGER.info(data)
 
-        return data, len(res.bindings)
+        return data, len(all_res.bindings)
 
     def get_title(self, obj_uri):
         """
@@ -346,13 +477,13 @@ OFFSET {offset}
         :return:
         """
 
-        prefixes = 'PREFIX ' + '\nPREFIX '.join(TITLE_PREFIXES)
+        prefixes = 'PREFIX ' + '\nPREFIX '.join(cfg.TITLE_PREFIXES)
 
-        fields = '|'.join(TITLE_FIELDS)
+        fields = '|'.join(cfg.TITLE_FIELDS)
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_ALL_OBJECTS_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_ALL_OBJECTS_NAMESPACE)
 
-        sparql_query = TITLE_QUERY_LANG.format(
+        sparql_query = cfg.TITLE_QUERY_LANG.format(
             uri=obj_uri,
             prefix=prefixes,
             fields=fields,
@@ -363,7 +494,7 @@ OFFSET {offset}
         if len(res.bindings) > 0:
             title = res.bindings[0]['title'].value
         else:
-            sparql_query = TITLE_QUERY.format(
+            sparql_query = cfg.TITLE_QUERY.format(
                     uri=obj_uri,
                     prefix=prefixes,
                     fields=fields,
@@ -384,13 +515,13 @@ OFFSET {offset}
         :return:
         """
 
-        prefixes = 'PREFIX ' + '\nPREFIX '.join(DESCRIPTION_PREFIXES)
+        prefixes = 'PREFIX ' + '\nPREFIX '.join(cfg.DESCRIPTION_PREFIXES)
 
-        fields = '|'.join(DESCRIPTION_FIELDS)
+        fields = '|'.join(cfg.DESCRIPTION_FIELDS)
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_ALL_OBJECTS_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_ALL_OBJECTS_NAMESPACE)
 
-        sparql_query = TITLE_QUERY_LANG.format(
+        sparql_query = cfg.TITLE_QUERY_LANG.format(
             uri=obj_uri,
             prefix=prefixes,
             fields=fields,
@@ -401,7 +532,7 @@ OFFSET {offset}
         if len(res.bindings) > 0:
             desc = res.bindings[0]['title'].value
         else:
-            sparql_query = TITLE_QUERY.format(
+            sparql_query = cfg.TITLE_QUERY.format(
             uri=obj_uri,
             prefix=prefixes,
             fields=fields,
@@ -421,7 +552,7 @@ OFFSET {offset}
         :param obj_uri:
         :return:
         """
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_ALL_OBJECTS_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_ALL_OBJECTS_NAMESPACE)
 
         sparql_query = """
                     PREFIX dct:<http://purl.org/dc/terms/>
@@ -451,15 +582,15 @@ OFFSET {offset}
 
         # todo: not tested yet
 
-        sparql_query = TITLE_QUERY_LANG
+        sparql_query = cfg.TITLE_QUERY_LANG
 
-        prefixes = 'PREFIX ' + '\nPREFIX '.join(LABEL_PREFIXES)
+        prefixes = 'PREFIX ' + '\nPREFIX '.join(cfg.LABEL_PREFIXES)
 
-        fields = '|'.join(LABEL_FIELDS)
+        fields = '|'.join(cfg.LABEL_FIELDS)
 
-        sparql = self.tripel_store.sparql_for_namespace(PLONE_ALL_OBJECTS_NAMESPACE)
+        sparql = self.tripel_store.sparql_for_namespace(cfg.PLONE_ALL_OBJECTS_NAMESPACE)
 
-        sparql_query_de = TITLE_QUERY_LANG.format(
+        sparql_query_de = cfg.TITLE_QUERY_LANG.format(
             uri=label_uri,
             prefix=prefixes,
             fields=fields,
@@ -471,7 +602,7 @@ OFFSET {offset}
             label = res_de.bindings[0]['title'].value
             return label
 
-        sparql_query_en = TITLE_QUERY_LANG.format(
+        sparql_query_en = cfg.TITLE_QUERY_LANG.format(
             uri=label_uri,
             prefix=prefixes,
             fields=fields,
@@ -494,7 +625,7 @@ OFFSET {offset}
         # todo: real query
         query = 'CONSTRUCT  WHERE { ?s ?p ?o }'
 
-        data = self.tripel_store.get_turtle_from_query(PLONE_ALL_OBJECTS_NAMESPACE, query)
+        data = self.tripel_store.get_turtle_from_query(cfg.PLONE_ALL_OBJECTS_NAMESPACE, query)
 
         return data
 
