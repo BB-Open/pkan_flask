ead72506a7b7009eab9deb2617084efc890e5b32
sandra <sandra.rum@inqbus.de>
2020-01-27 11:39:11 +0100

    Added response_codes and error_messages. Added better Error-Handling

--- src/pkan/flask/configs/config_default.py
+++ src/pkan/flask/configs/config_default.py
@@ -75,3 +75,8 @@ TITLE_QUERY = """
                      <{uri}> {fields} ?title.
                     }}
                 """
+
+BAD_REQUEST = 400
+INTERNAL_SERVER_ERROR = 500
+INTERNAL_SERVER_ERROR_MSG = 'Es gab einen internen Serverfehler. Versuchen Sie die Seite neu zu laden oder wenden Sie sich an den Admin der Seite'
+REQUEST_OK = 200
