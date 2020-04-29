bc71276ef559f635d9ca75c48a95e4b3b6780458
volker <volker.jaenisch@inqbus.de>
2020-04-29 18:50:04 +0200

    corrected startup

--- flask_run.py
+++ flask_run.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-"""Simple Flask bases websocket demo."""
 
 # Initialize Flask.
-# need import for routing
-import pkan.flask.routing
 from pkan.flask.routes import app
 
 if __name__ == '__main__':
