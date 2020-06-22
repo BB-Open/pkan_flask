c9b64cb957ce9bf1d2b7dc5d346ea93e9b118961
sandra <sandra.rum@inqbus.de>
2020-06-22 16:25:55 +0200

    Pep8

--- src/pkan/flask/periodic.py
+++ src/pkan/flask/periodic.py
@@ -1,3 +1,9 @@
+"""Scheduler for Cronjobs"""
+import time
+import requests
+from requests.auth import HTTPBasicAuth
+import schedule
+
 from pkan.flask.log import LOGGER
 
 try:
@@ -5,26 +11,35 @@ try:
 except ImportError:
     import pkan.flask.configs.config_default as cfg
 
-import requests
-from requests.auth import HTTPBasicAuth
-import schedule
-import time
-
 
 def plone_harvest():
+    """
+    Harvest Plone Objects and transfer dem in sparql-store
+    :return:
+    """
     LOGGER.info('Initiate harvesting')
-    response = requests.get(cfg.HARVEST_URL,  auth=HTTPBasicAuth(cfg.HARVEST_USER, cfg.HARVEST_PASS))
+    response = requests.get(cfg.HARVEST_URL, auth=HTTPBasicAuth(cfg.HARVEST_USER, cfg.HARVEST_PASS))
     if response.status_code == 200:
         LOGGER.info('Harvesting initiated')
     else:
-        LOGGER.warning('Failed to initiate Harvesting. Error Code: %s' % response)
+        LOGGER.warning('Failed to initiate Harvesting. Error Code: %s', response)
+
 
 def scheduler_loop():
+    """
+    Main Loop
+    :return:
+    """
     while True:
         schedule.run_pending()
-        time.sleep()
+        time.sleep(5)
+
 
 def start_scheduler():
+    """
+    start the scheduler
+    :return:
+    """
     LOGGER.info('Initializing the scheduler')
     schedule.every(cfg.HARVEST_PERIOD).seconds.do(plone_harvest)
     LOGGER.info('Scheduler intitialized')
@@ -32,5 +47,6 @@ def start_scheduler():
     LOGGER.info('Starting scheduler loop')
     scheduler_loop()
     LOGGER.info('Scheduler loop started')
+
 
 start_scheduler()
