b3c3715d82d00c23dceeb6669f9ad5330641dece
sandra <sandra.rum@inqbus.de>
2020-06-22 16:08:04 +0200

    Pep8

--- src/pkan/flask/routes.py
+++ src/pkan/flask/routes.py
@@ -1,15 +1,18 @@
 """
     Websocket Api
 """
+
+
 import functools
 import sys
 import traceback
+import logging
 
+from flask import Flask, request, jsonify
 from flask import send_file
+from flask_cors import CORS
 
 import simplejson as sj
-from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 from pkan.flask.log import LOGGER
 from pkan.flask.sparql_db import DBManager
@@ -19,7 +22,6 @@ try:
 except ImportError:
     import pkan.flask.configs.config_default as cfg
 
-import logging
 
 logging.getLogger('flask_cors').level = logging.DEBUG
 
@@ -31,6 +33,11 @@ DB_MANAGER = DBManager()
 
 
 def pkan_status(f):
+    """
+    Wrapper to give the status code
+    :param f:
+    :return:
+    """
     @functools.wraps(f)
     def wrapped(data=None):
         res_data = {}
@@ -38,20 +45,25 @@ def pkan_status(f):
         res_data['error_message'] = ''
         try:
             res_data.update(f(data=data))
-        except Exception as e:
+        except Exception as _e:
             res_data['response_code'] = cfg.INTERNAL_SERVER_ERROR
             res_data['error_message'] = cfg.INTERNAL_SERVER_ERROR_MSG
             exc_type, exc_value, exc_traceback = sys.exc_info()
-            LOGGER.error("Request failed with Error %s %s " % (exc_type, exc_value))
+            LOGGER.error("Request failed with Error %s %s ", exc_type, exc_value)
             for line in traceback.format_tb(exc_traceback):
-                LOGGER.error("%s" % (line[:-1]))
+                LOGGER.error("%s", line[:-1])
         return(sj.dumps(res_data))
+
     return wrapped
 
 # Download
 
 @app.route('/download')
 def return_files_tut():
+    """
+    Download Files
+    :return:
+    """
     params = {}
     # id empty means full export
     params['id'] = request.args.get('id', default=None, type=str)
@@ -59,7 +71,7 @@ def return_files_tut():
     params['type'] = request.args.get('type', default='tree', type=str)
     # ignore on type tree, use on type graph
     params['count'] = request.args.get('count', default='3', type=int)
-    file_path, file_name, file, mimetype = DB_MANAGER.get_download_file(params)
+    file_path, file_name, _file, mimetype = DB_MANAGER.get_download_file(params)
     try:
         return send_file(file_path, attachment_filename=file_name, mimetype=mimetype)
     except Exception as e:
@@ -91,8 +103,8 @@ def request_vocab(data=None):
         data['vocab'] = DB_MANAGER.get_publisher_vocab()
     else:
         data['vocab'] = DB_MANAGER.get_sorting_options()
-    LOGGER.info('request %s finished' % params)
-    LOGGER.info('response is %s ' % data)
+    LOGGER.info('request %s finished', params)
+    LOGGER.info('response is %s ', data)
     return jsonify(data)
 
 
@@ -110,17 +122,17 @@ def request_search_results(data=None):
     data = {}
     try:
         data['results'], data['result_count'] = DB_MANAGER.get_search_results(params)
-    except Exception as e:
+    except Exception as _e:
         exc_type, exc_value, exc_traceback = sys.exc_info()
-        LOGGER.error("Failed with Error %s %s " % (
-            exc_type, exc_value))
+        LOGGER.error("Failed with Error %s %s ",
+                     exc_type, exc_value)
         for line in traceback.format_tb(exc_traceback):
-            LOGGER.error("Trace: %s" % (line[:-1]))
+            LOGGER.error("Trace: %s", line[:-1])
         # ToDo: Fix unspecified Exception catch
         data['results'], data['result_count'] = [], 0
     data['batch_start'] = params['batch_start']
     data['batch_end'] = params['batch_end']
-    LOGGER.info('request_search_results finished: %s ' % data)
+    LOGGER.info('request_search_results finished: %s ', data)
     return jsonify(data)
 
 @app.route('/request_search_results_sparql', methods=['POST'])
@@ -135,7 +147,8 @@ def request_search_results_sparql(data=None):
 
     LOGGER.info(params)
     data = {}
-    data['results'], data['result_count'], data['error_message'] = DB_MANAGER.get_search_results_sparql(params)
+    data['results'], data['result_count'], data['error_message'] = \
+        DB_MANAGER.get_search_results_sparql(params)
     if data['error_message']:
         LOGGER.info(data['error_message'])
         data['response_code'] = cfg.BAD_REQUEST
