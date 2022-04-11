b6042c788d5b0a7c617a2170c80ead228dfd5a93
sandra <sandra.rum@inqbus.de>
2022-04-11 11:23:47 +0200

    Added solr to scheduler

--- src/pkan/flask/periodic.py
+++ src/pkan/flask/periodic.py
@@ -1,32 +1,46 @@
 """Scheduler for Cronjobs"""
+import sys
 import time
-import requests
-from requests.auth import HTTPBasicAuth
-import schedule
+from traceback import format_tb
 
+import schedule
+from iso2dcat.solr.rdf2solr import main as solr_main
 from pkan.flask.log import LOGGER
 from pkan_config.config import get_config
+
 
 def plone_harvest():
     """
     Harvest Plone Objects and transfer dem in sparql-store
     :return:
     """
-    cfg = get_config()
+    # cfg = get_config()
+    #
+    # LOGGER.info('Initiate harvesting')
+    # try:
+    #     response = requests.get(cfg.HARVEST_URL, auth=HTTPBasicAuth(cfg.HARVEST_USER, cfg.HARVEST_PASS))
+    # except requests.exceptions.ConnectionError:
+    #     # retry one time
+    #     response = requests.get(cfg.HARVEST_URL, auth=HTTPBasicAuth(cfg.HARVEST_USER, cfg.HARVEST_PASS))
+    #
+    # if response.status_code == 200:
+    #     LOGGER.info('Harvesting initiated')
+    #     LOGGER.info('Harvesting Response is: %s', response)
+    # else:
+    #     LOGGER.warning('Failed to initiate Harvesting. Error Code: %s', response)
 
-    LOGGER.info('Initiate harvesting')
+    LOGGER.info('Start Solr Export')
     try:
-        response = requests.get(cfg.HARVEST_URL, auth=HTTPBasicAuth(cfg.HARVEST_USER, cfg.HARVEST_PASS))
-    except requests.exceptions.ConnectionError:
-        # retry one time
-        response = requests.get(cfg.HARVEST_URL, auth=HTTPBasicAuth(cfg.HARVEST_USER, cfg.HARVEST_PASS))
+        solr_main()
+    except Exception:
+        LOGGER.error('Could not write to Solr')
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        msg = u"GET termiated due to error %s %s" % (exc_type, exc_value)
+        LOGGER.error(msg)
+        for line in format_tb(exc_traceback):
+            LOGGER.error(line)
 
-    if response.status_code == 200:
-        LOGGER.info('Harvesting initiated')
-        LOGGER.info('Harvesting Response is: %s', response)
-    else:
-        LOGGER.warning('Failed to initiate Harvesting. Error Code: %s', response)
-
+    LOGGER.info('Finished Solr Export')
 
 def scheduler_loop():
     """
